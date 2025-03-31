af9be17fb352198e3bb47d37b9b621d8d8820291
Aleksandr Zelukin <92846541+AleksandrZelukin@users.noreply.github.com>
2025-03-31 17:07:40 +0300

    Update apps

--- agenti.py
+++ agenti.py
@@ -1,2 +1,37 @@
 import csv
-#print(dir(csv))
+
+# with open('agenti.csv', mode ='r')as file:
+#   csvFile = csv.reader(file)
+#   for lines in csvFile:
+#         print(lines)
+
+
+with open('agenti.csv', mode='r') as file:
+    csv_reader = csv.DictReader(file)
+    for row in csv_reader:
+        print(row)
+
+
+data = [
+    ['Name', 'Age', 'City'],
+    ['Alice', '30', 'New York'],
+    ['Bob', '25', 'Los Angeles'],
+    ['Charlie', '35', 'Chicago']
+]
+
+with open('output.csv', mode='w', newline='') as file:
+    csv_writer = csv.writer(file)
+    csv_writer.writerows(data)
+
+
+data = [
+    {'Name': 'Alice', 'Age': '30', 'City': 'New York'},
+    {'Name': 'Bob', 'Age': '25', 'City': 'Los Angeles'},
+    {'Name': 'Charlie', 'Age': '35', 'City': 'Chicago'}
+]
+
+with open('output.csv', mode='w', newline='') as file:
+    fieldnames = ['Name', 'Age', 'City']
+    csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
+    csv_writer.writeheader()
+    csv_writer.writerows(data)
