0f266d40da8524f8993217434551f410ddba0f37
Aleksandr Zelukin <92846541+AleksandrZelukin@users.noreply.github.com>
2025-03-31 21:26:56 +0300

    Update apps

--- agenti.py
+++ agenti.py
@@ -1,3 +1,4 @@
+# https://www.squash.io/processing-csv-files-in-python/
 import csv
 
 # with open('agenti.csv', mode ='r')as file:
@@ -12,26 +13,10 @@ with open('agenti.csv', mode='r') as file:
         print(row)
 
 
-data = [
-    ['Name', 'Age', 'City'],
-    ['Alice', '30', 'New York'],
-    ['Bob', '25', 'Los Angeles'],
-    ['Charlie', '35', 'Chicago']
-]
-
-with open('output.csv', mode='w', newline='') as file:
-    csv_writer = csv.writer(file)
-    csv_writer.writerows(data)
-
-
-data = [
-    {'Name': 'Alice', 'Age': '30', 'City': 'New York'},
-    {'Name': 'Bob', 'Age': '25', 'City': 'Los Angeles'},
-    {'Name': 'Charlie', 'Age': '35', 'City': 'Chicago'}
-]
-
-with open('output.csv', mode='w', newline='') as file:
-    fieldnames = ['Name', 'Age', 'City']
-    csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
-    csv_writer.writeheader()
-    csv_writer.writerows(data)
+with open("agenti.csv", "r") as file:
+    csv_reader = csv.reader(file)
+    headers = next(csv_reader)
+    for row in csv_reader:
+        pilseta = int(row[3])
+        if pilseta == "Rīga":
+            print(row)
---
+++ api_5.py
@@ -0,0 +1,28 @@
+import csv
+# Writing to a CSV file using csv.writer
+# Writing to a CSV file using csv.DictWriter
+
+
+data = [
+    ['Name', 'Age', 'City'],
+    ['Alice', '30', 'New York'],
+    ['Bob', '25', 'Los Angeles'],
+    ['Charlie', '35', 'Chicago']
+]
+
+with open('api_5.csv', mode='w', newline='') as file:
+    csv_writer = csv.writer(file)
+    csv_writer.writerows(data)
+
+
+data = [
+    {'Name': 'Alice', 'Age': '30', 'City': 'New York'},
+    {'Name': 'Bob', 'Age': '25', 'City': 'Los Angeles'},
+    {'Name': 'Charlie', 'Age': '35', 'City': 'Chicago'}
+]
+
+with open('api_5.csv', mode='w', newline='') as file:
+    fieldnames = ['Name', 'Age', 'City']
+    csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
+    csv_writer.writeheader()
+    csv_writer.writerows(data)
