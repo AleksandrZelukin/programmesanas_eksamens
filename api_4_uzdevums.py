13decdaa7ba58fd0edb467dee365e40c829d67bc
Aleksandr Zelukin <alexandr44@inbox.lv>
2025-11-28 12:28:08 +0200

    Create apps

---
+++ api_4_uzdevums.py
@@ -0,0 +1,7 @@
+import requests
+import json
+
+url = requests.get("https://pro2025.azurewebsites.net/journals")
+
+url = requests.get("https://pro2025.azurewebsites.net/journals?year_established=2000")
+
